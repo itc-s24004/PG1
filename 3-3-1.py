af992975fce86d7d641eaaace95582dba94b4022
伊波 健之介 <s24004@std.it-college.ac.jp>
2024-07-09 08:00:48 +0000

    2024_07_09_p87~118

---
+++ 3-3-1.py
@@ -0,0 +1,7 @@
+my_list = ['tokyo', 'osaka', 'fukuoka', 'aichi', 'kyoto', 'chiba', 'saitama', 'gunma']
+my_list_6 = []
+for t in my_list:
+    if len(t) >= 6:
+        my_list_6.append(t)
+
+print(my_list_6)
