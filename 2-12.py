bf896647c0724f9cf29a631e3a47ba9ab27f0d94
伊波 健之介 <s24004@std.it-college.ac.jp>
2024-07-09 06:34:43 +0000

    2024_07_09

---
+++ 2-12.py
@@ -0,0 +1,10 @@
+import random 
+import my_module
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+print(random.choice(alphabet))
+
+print(random.sample(alphabet, 5))
+
+print(my_module.func(5))
---
+++ 2-13-1.py
@@ -0,0 +1,6 @@
+import random as rdm
+from random import randint as rdt
+
+print(rdm.randint(0, 10))
+
+print(rdt(0, 10))
