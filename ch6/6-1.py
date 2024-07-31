bddf6c8de6aeb32f9345052eb443b1c357db92a1
s24004 <s24004@std.it-college.ac.jp>
2024-07-31 14:51:25 +0900

    ch2整理

---
+++ ch6/6-1.py
@@ -0,0 +1,15 @@
+class Car:
+  weight = 4000
+  num_wheels = 4
+
+  def __init__(self, car_name="NoName") :
+    self.name = car_name
+  
+  def calc_weight_per_wheel(self):
+    return self.weight / self.num_wheels
+  
+default_car = Car()
+print(default_car.name)
+
+my_car = Car("DeLorean")
+print(my_car.name)
