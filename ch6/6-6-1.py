0374367c2e4508196c325823e2f2d7b269c01f3b
s24004 <s24004@std.it-collgege.ac.jp>
2024-08-06 17:00:52 +0900

    ch6_08_06

---
+++ ch6/6-6-1.py
@@ -0,0 +1,37 @@
+x = "Happy "
+
+def event1():
+  print("In event1:", x, end=" -> ")
+
+  
+def event2():
+  x= "Sad   "
+  print("In event12", x, end=" -> ")
+
+
+def event3():
+  global x
+  x= "Tired  "
+  print("In event3", x, end=" -> ")
+
+
+def event4():
+  x= "Excite"
+  def happening():
+    print("in event4:", x, end=" -> ")
+  happening()
+
+
+def event5():
+  x = "Fun    "
+  def happening():
+    nonlocal x
+    x = "Scare "
+  happening()
+  print("In event5:", x, end=" -> ")
+
+func_list = [ event1, event2, event3, event4, event5]
+
+for f in func_list:
+  f()
+  print(f"After {f.__name__}: {x}")
